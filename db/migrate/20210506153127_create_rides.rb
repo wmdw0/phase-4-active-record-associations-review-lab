90972ddd4822e50fdaea950da1dd4aaeb62a4e78
William Walsh <wmdwalsh0@gmail.com>
2021-12-13 00:47:14 -0500

    completed assignment

--- db/migrate/20210506153127_create_rides.rb
+++ db/migrate/20210506153127_create_rides.rb
@@ -2,6 +2,9 @@ class CreateRides < ActiveRecord::Migration[6.1]
   def change
     create_table :rides do |t|
 
+      t.belongs_to :taxi, null: false, foreign_key: true
+      t.belongs_to :passenger, null: false, foreign_key: true
+
       t.timestamps
     end
   end
